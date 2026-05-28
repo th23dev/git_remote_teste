ee1585e66a9f17f90f8967b09160c0dd24f770d7
th23dev <thaua23sl@gmail.com>
2026-05-28 10:20:54 -0300

    update calculator

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,22 @@
 num1 = int(input("Digite o primeiro número: "))
 num2 = int(input("Digite o segundo número: "))
 
-print(num1, "+", num2, "=", num1 + num2)
-print(num1, "-", num2, "=", num1 - num2)
+print("Escolha a operação:")
+print("1 - Adição")
+print("2 - Subtração")
+print("3 - Multiplicação")
+print("4 - Divisão")
+opcao = int(input("Digite a opção (1/2/3/4): "))
+
+if opcao == 1:
+    resultado = num1 + num2
+    print(f"O resultado da adição é: {resultado}")
+elif opcao == 2:
+    resultado = num1 - num2
+    print(f"O resultado da subtração é: {resultado}")
+elif opcao == 3:
+    resultado = num1 * num2
+    print(f"O resultado da multiplicação é: {resultado}")
+elif opcao == 4:
+    resultado = num1 / num2
+    print(f"O resultado da divisão é: {resultado}")
